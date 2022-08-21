d9f26e1393347343331bf2e9582a96c440b718e7
Martin <martin.n.sarubbi@hotmail.com>
2022-08-21 14:34:10 -0300

    Updated metro.config.js file

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,29 @@
-// Learn more https://docs.expo.io/guides/customizing-metro
-const { getDefaultConfig } = require('expo/metro-config');
+/**
+ * Metro configuration for React Native
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
 
-module.exports = getDefaultConfig(__dirname);
+ const { getDefaultConfig } = require("metro-config");
+
+ module.exports = (async () => {
+   const {
+     resolver: { assetExts }
+   } = await getDefaultConfig();
+ 
+   return {
+     transformer: {
+       getTransformOptions: async () => ({
+         transform: {
+           experimentalImportSupport: false,
+           inlineRequires: true,
+         },
+       }),
+     },
+     resolver: {
+       assetExts: [...assetExts, "obj", "mtl", "JPG", "vrx", "hdr", "gltf", "glb", "bin", "arobject", "gif"]
+     }
+   }
+ })();
+ 
